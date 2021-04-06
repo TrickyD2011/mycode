7a02d0dc2755eaf609bbed9ba9f6030f03974e22
TrickyD2011 <student@bchd.7934b0ae-94fb-4b58-8532-dba886f436ca>
2021-04-06 21:11:56 +0000

    touching character part 1

--- mainworkline/exampleAPI.py
+++ mainworkline/exampleAPI.py
@@ -4,7 +4,7 @@
 import requests
 
 #messages
-messge = 'What would you like to play?'
+message = 'What DND class do you want to know about?'
 
 def main():
 
@@ -19,23 +19,47 @@ def main():
     respdata = resp.json()
 
     # display ALL of the data we captured
-    print(respdata)
+    #print(respdata)
 
 
     # spend some time working with dataset
     # see if you can return the data in an interesting format
     # (make it more readable)
 
+    #Starting
+    print(message)
+    answer = input('Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard? \n' )
+    
+
     #Test Prints
-    #print(*Class["Caster"], sep=", " ) #sep=", "
-    #print(*Class["Martial"], sep=", ")
-    #print(*Class["Hybrid"], sep=", ")
+    if answer.lower()=="barbarian":
+        print(respdata.get('results')[0]["name"] ) #Barbarian
+    elif answer.lower()== "bard":
+        print(respdata.get('results')[1]["name"] ) #Bard
+    elif answer.lower()== "cleric":    
+        print(respdata.get('results')[2]["name"] ) #Cleric
+    elif answer.lower()== "druid":    
+        print(respdata.get('results')[3]["name"] ) #Druid
+    elif answer.lower()== "fighter":    
+        print(respdata.get('results')[4]["name"] ) #Fighter
+    elif answer.lower()== "monk":    
+        print(respdata.get('results')[5]["name"] ) #Monk
+    elif answer.lower()== "paladin":    
+        print(respdata.get('results')[6]["name"] ) #Paladin
+    elif answer.lower()== "ranger":    
+        print(respdata.get('results')[7]["name"] ) #Ranger
+    elif answer.lower()== "rogue":    
+        print(respdata.get('results')[8]["name"] ) #Rogue
+    elif answer.lower()== "sorcerer":    
+        print(respdata.get('results')[9]["name"] ) #Sorcerer
+    elif answer.lower()== "warlock":    
+        print(respdata.get('results')[10]["name"] ) #Warlock
+    elif answer.lower()== "wizard":    
+        print(respdata.get('results')[11]["name"] ) #Wizard
+    else:
+        print("Thank you for choosing.")
 
-    #getting info and supplying info
-#    print(message)
-#    choice = input("Caster, Martial, or Hybrid?")
 
-   # if choice="Caster":
-#        print("Here is a list of casters "+ *Class["Caster"], sep=", ")
+
 
 main()
