019673ab223635e7d2f60019d0c42b798f245843
TrickyD2011 <student@bchd.7934b0ae-94fb-4b58-8532-dba886f436ca>
2021-04-07 20:14:29 +0000

    Python and physical interfaces

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-round = 0
-answer = " "
+"""System variables"""
+ROUND = 0
+ANSWER = " "
 mylist = ('brian', 'shrubbery')
 
-while round < 3 and answer.lower() not in mylist:
-    round += 1     # increase the round counter by 1
-    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    if answer.lower() == "brian": # logic to check if user gave correct answer
+"""System Module"""
+while ROUND < 3 and ANSWER.lower() not in mylist:
+    ROUND += 1     # increase the round counter by 1
+    ANSWER = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
+    if ANSWER.lower() == "brian": # logic to check if user gave correct answer
         print("Correct!")
-    elif round == 3:    # logic to ensure round has not yet reached 3
+    elif ROUND == 3:    # logic to ensure round has not yet reached 3
         print("Sorry, the answer was Brian.")
-    elif answer.lower() == "shrubbery": #secret answer
+    elif ANSWER.lower() == "shrubbery": #secret answer
         print("You gave the super secret answer!")
     else:                 # if answer was wrong
         print("Sorry. Try again!")
-
