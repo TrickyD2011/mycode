12680d9a7b4ab85abc332d7911620f4ddc26bdd7
TrickyD2011 <student@bchd.7934b0ae-94fb-4b58-8532-dba886f436ca>
2021-04-08 14:16:28 +0000

    10/10 Pie

--- graphing/thursdaygraph.py
+++ graphing/thursdaygraph.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 """Imports"""
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 
 
-"""Main Code"""
-
 
 def main():
-    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-    y = np.array([12, 35, 3, 50])
+    """Time to make a Pie Chart"""
+    # Size of each slice:
+    size = np.array([12, 35, 3, 50])
+    # Label of each slice:
     mylabels = ["Perfect Slice", "Too Big", "Too Little", "The Rest of the Pie"]
     # makes slice come out
     myexplode = [0.2, 0, 0, 0]
     # Customize the pie
-    plt.pie(y, labels = mylabels, explode = myexplode, shadow = True, startangle = 90)
+    plt.pie(size, labels = mylabels, explode = myexplode, shadow = True, startangle = 90)
     # Create a legend
     plt.legend(title = "The Right Amount of Pie:")
     # display/save the graph
     plt.savefig("/home/student/mycode/graphing/perfectpy.png")
-    # copy
+    # copy of save
     plt.savefig("/home/student/static/perfectpy.png")
 
 
